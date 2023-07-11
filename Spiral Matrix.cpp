b45b5dda767e7f0f72706215c4fd90ecdc72771e
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-07-11 20:30:27 +0530

    Create Spiral Matrix.cpp

---
+++ Spiral Matrix.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h> 
+vector<int> spiralPathMatrix(vector<vector<int>> matrix, int n, int m) 
+{
+    // Write your code here.
+    vector<int> ans;
+    int top=0,left=0,bottom=n-1,right=m-1;
+    while(top<=bottom && left<=right){
+        for(int i=left;i<=right;i++){
+            ans.push_back(matrix[top][i]);
+        }
+        top++;
+
+        for(int i=top;i<=bottom;i++){
+            ans.push_back(matrix[i][right]);
+        }
+        right--;
+
+        if(top<=bottom){
+            for(int i=right;i>=left;i--){
+                ans.push_back(matrix[bottom][i]);
+            }
+            bottom--;
+        }
+
+        if(left<=right){
+            for(int i=bottom;i>=top;i--){
+                ans.push_back(matrix[i][left]);
+            }
+            left++;
+        }
+    }
+    return ans;
+}

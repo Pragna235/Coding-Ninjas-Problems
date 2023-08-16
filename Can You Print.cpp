8951a3b25abaff9d4ffb14c34a029ee2d81d39bc
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-08-16 19:04:00 +0530

    Create Can You Print.cpp

---
+++ Can You Print.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h> 
+vector < int > canYouPrint(int n) {
+    // Write your code here.
+    vector<int> result;
+    for(int i=1;i<=n;i++){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    return result;
+}

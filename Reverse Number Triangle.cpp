40620f9d6801b2be75cc334d68c139ede827cb14
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-02-06 18:54:43 +0530

    Create Reverse Number Triangle.cpp

---
+++ Reverse Number Triangle.cpp
@@ -0,0 +1,9 @@
+void nNumberTriangle(int n) {
+    // Write your code here.
+    for(int i=n;i>0;i--){
+        for(int j=1;j<=i;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}

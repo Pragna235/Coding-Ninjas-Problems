4bed1cad0658bf8b8ee2c34215acaa9375d718cd
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-08-05 18:54:10 +0530

    Create Intersection of Two Sorted Arrays.cpp

---
+++ Intersection of Two Sorted Arrays.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h> 
+vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
+{
+	// Write your code here.
+	int i=0;
+	int j=0;
+	vector<int> intersection;
+
+	while(i<n && j<m){
+		if(arr1[i] == arr2[j]){
+			intersection.push_back(arr1[i]);
+			i++;
+			j++;
+		}
+		else if(arr1[i]>arr2[j]){
+			j++;
+		}
+		else{
+			i++;
+		}
+	}
+
+	return intersection;
+}

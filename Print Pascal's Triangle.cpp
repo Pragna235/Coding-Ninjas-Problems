3d6d08d6a48a28ebcd20b6ed278a7d1c3385f729
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-08-16 11:36:36 +0530

    Create Print Pascal's Triangle.cpp

---
+++ Print Pascal's Triangle.cpp
@@ -0,0 +1,20 @@
+vector<int> generateRow(int row){
+    long long ans=1;
+    vector<int> ansRow;
+    ansRow.push_back(1);
+    for(int col=1;col<row;col++){
+        ans = ans * (row-col);
+        ans = ans /(col);
+        ansRow.push_back(ans);
+    }
+    return ansRow;
+}
+
+vector<vector<int>> pascalTriangle(int N) {
+    // Write your code here.
+    vector<vector<int>> ans;
+    for(int i=1;i<=N;i++){
+        ans.push_back(generateRow(i));
+    }
+    return ans;
+}

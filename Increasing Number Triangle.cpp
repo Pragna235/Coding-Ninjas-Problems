ce47012220e5cd9457b257476e347886b062b1c2
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-08-17 19:02:39 +0530

    Create Increasing Number Triangle.cpp

---
+++ Increasing Number Triangle.cpp
@@ -0,0 +1,11 @@
+void nNumberTriangle(int n) {
+    // Write your code here.
+    int num=1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<num<<" ";
+            num+=1;
+        }
+        cout<<endl;
+    }
+}

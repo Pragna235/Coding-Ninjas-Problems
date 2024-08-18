6873172f01bbc1913fcd183a86c85a1d8eb53990
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-08-18 16:30:21 +0530

    Create Number of Inversions.cpp

---
+++ Number of Inversions.cpp
@@ -0,0 +1,44 @@
+int cnt=0;
+
+void merge(vector<int> &arr, int low, int mid, int high){
+    int left=low,right=mid+1;
+    vector<int>temp;
+    while(left<=mid && right<=high){
+        if(arr[left]<=arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else{
+            temp.push_back(arr[right]);
+            cnt+=(mid-left+1);
+            right++;
+        }
+    }
+    while(left<=mid){
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while(right<=high){
+        temp.push_back(arr[right]);
+        right++;
+    }
+    for(int i=low;i<=high;i++){
+        arr[i] = temp[i-low];
+    }
+}
+
+void mergeSort(vector<int> &arr, int low, int high){
+    if(low>=high) return;
+    int mid = (low+high)/2;
+    mergeSort(arr,low,mid);
+    mergeSort(arr,mid+1,high);
+    merge(arr,low,mid,high);
+}
+
+
+int numberOfInversions(vector<int>&a, int n) {
+    // Write your code here.
+    mergeSort(a,0,n-1);
+    return cnt;
+
+}

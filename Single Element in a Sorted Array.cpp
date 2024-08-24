4d0af136a64da3ea754413df5bedf8e646b2a1c9
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-08-24 15:51:43 +0530

    Create Single Element in a Sorted Array.cpp

---
+++ Single Element in a Sorted Array.cpp
@@ -0,0 +1,21 @@
+int singleNonDuplicate(vector<int>& arr)
+{
+	// Write your code here
+	int n = arr.size();
+	if(n==1) return arr[0];
+	if(arr[0] != arr[1]) return arr[0];
+	if(arr[n-1] !=arr[n-2]) return arr[n-1];
+	int low = 1, high = n-2;
+	while(low<=high){
+		int mid = (low+high)/2;
+		if(arr[mid]!=arr[mid-1] && arr[mid]!=arr[mid+1]) return arr[mid];
+
+		if((mid%2!=0 && arr[mid]==arr[mid-1]) || (mid%2==0 && arr[mid]==arr[mid+1])){
+			low=mid+1;
+		}
+		else{
+			high=mid-1;
+		}
+	}
+	return -1;
+}

777bf6a149da3aa1a604ba5805c214adf63fc4a5
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-08-24 14:00:24 +0530

    Create  Search In Rotated Sorted Array.cpp

---
+++ Search In Rotated Sorted Array.cpp
@@ -0,0 +1,28 @@
+int search(vector<int>& arr, int n, int k)
+{
+    // Write your code here.
+    // Return the position of K in ARR else return -1.
+    int low=0,high=n-1;
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(arr[mid]==k) return mid;
+        
+        if(arr[low]<=arr[mid]){
+            if(arr[low]<=k && k<=arr[mid]){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        else{
+            if(arr[mid]<=k && k<=arr[high]){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+    }
+    return -1;
+}

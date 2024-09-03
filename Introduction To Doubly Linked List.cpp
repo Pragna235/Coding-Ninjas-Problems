5cc33b9264050a2e9d77a2556510f0547d12fef3
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-09-03 14:15:07 +0530

    Create Introduction To Doubly Linked List.cpp

---
+++ Introduction To Doubly Linked List.cpp
@@ -0,0 +1,24 @@
+/*
+ * Definition for doubly-linked list.
+ * class Node
+ * {
+ * public:
+ *    int data;
+ *    Node *next, *prev;
+ *    Node() : data(0), next(nullptr), prev(nullptr) {}
+ *    Node(int x) : data(x), next(nullptr), prev(nullptr) {}
+ *    Node(int x, Node *next, Node *prev) : data(x), next(next), prev(prev) {}
+ * };
+ */
+
+Node* constructDLL(vector<int>& arr) {
+    // Write your code here
+    Node* head = new Node(arr[0]);
+    Node* prev = head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp = new Node(arr[i],nullptr,prev);
+        prev->next = temp;
+        prev = temp;
+    }
+    return head;
+}

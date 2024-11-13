fa7fe6ed96970efef38e6b839c4506f40df57f55
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-11-13 05:39:13 +0530

    Create Linked List Cycle II.cpp

---
+++ Linked List Cycle II.cpp
@@ -0,0 +1,48 @@
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node()
+            {
+                this->data = 0;
+                next = NULL;
+            }
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+            Node(int data, Node* next)
+            {
+                this->data = data;
+                this->next = next;
+            }
+        };
+
+
+*****************************************************************/
+
+Node *firstNode(Node *head)
+{
+    //    Write your code here.
+    Node* slow=head;
+    Node* fast = head;
+    while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast){
+            slow=head;
+            while(slow!=fast){
+                slow=slow->next;
+                fast=fast->next;
+            }
+            return slow;
+        }
+    }
+    return NULL;
+}
